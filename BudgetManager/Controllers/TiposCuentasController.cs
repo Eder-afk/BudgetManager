82938ab0de6caece39e7b1996f1481710a63e338
Eder-afk <ezapata.a03@gmail.com>
2025-06-11 10:20:34 -0500

    feat: add view, model and controller for TiposCuentas

---
+++ BudgetManager/Controllers/TiposCuentasController.cs
@@ -0,0 +1,24 @@
+﻿using BudgetManager.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BudgetManager.Controllers
+{
+    public class TiposCuentasController: Controller
+    {
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Crear(TipoCuenta tipoCuenta)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(tipoCuenta);
+            }
+
+            return View();
+        }
+    }
+}
---
+++ BudgetManager/Models/TipoCuenta.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using BudgetManager.Validations;
+
+namespace BudgetManager.Models
+{
+    public class TipoCuenta //: IValidatableObject
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [StringLength(maximumLength: 50, MinimumLength = 3, ErrorMessage = "La longitud del campo {0} debe estar entre {2} y {1}")]
+        //[PrimeraLetraMayuscula]
+        public string Nombre { get; set; }
+        public int UsuarioId { get; set; }
+        public int Orden {  get; set; }
+
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    if (Nombre != null || string.IsNullOrEmpty(Nombre))
+        //    {
+        //        var primeraLetra = Nombre[0].ToString();
+
+        //        if (primeraLetra != primeraLetra.ToUpper())
+        //        {
+        //            yield return new ValidationResult("la primera letra aaaaaaaa noes japerkis",
+        //                new[] { nameof(Nombre) });
+        //        }
+        //    }
+        //}
+    }
+}
